9b331e206320183f951d0332237da0897fc9e47e
kyok01 <kazutakayokoils@gmail.com>
2022-06-13 23:30:08 +0900

    temporary save

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,9 +1,29 @@
-import { Outlet } from "react-router-dom";
+import {useEffect, useState} from 'react';
+import { Link, Outlet } from "react-router-dom";
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  },[]);
   return (
     <div>
       <h2>Posts</h2>
+      {posts.map((post) => {
+        return (
+          <li key={post.id}>
+          <Link to={`/posts/${post.id}`}>
+            {post.id}:{post.title}
+          </Link>
+        </li>
+        );
+      })}
       <Outlet />
     </div>
   );
